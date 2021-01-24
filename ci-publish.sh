94efbffa6f777f814e8764547d190c55fbb92b84
Stuart Leeks <stuart@leeks.net>
2021-01-24 20:29:45 +0000

    Add publish scripts

---
+++ ci-publish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t vscode-go-by-example-devcontainer -f ./.devcontainer/Dockerfile ./.devcontainer
+docker run -v ${PWD}:/workspaces/vscode-go-by-example --workdir /workspaces/vscode-go-by-example vscode-go-by-example-devcontainer bash -c "yarn run publish"
