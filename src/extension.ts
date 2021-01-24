636a7ecdbea05ff2182554b5ec2fd73397d280ce
Stuart Leeks <stuart@leeks.net>
2021-01-24 20:02:59 +0000

    Update command name

--- src/extension.ts
+++ src/extension.ts
@@ -3,30 +3,13 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-import { QuickPickItem } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// // Use the console to output diagnostic information (console.log) and errors (console.error)
-	// // This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "vscode-go-by-example" is now active!');
-
-	// // The command has been defined in the package.json file
-	// // Now provide the implementation of the command with registerCommand
-	// // The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('vscode-go-by-example.helloWorld', () => {
-	// 	// The code you place here will be executed every time your command is executed
-
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World from vscode-go-by-example!');
-	// });
-
-	// context.subscriptions.push(disposable);
-
 	context.subscriptions.push(
-		vscode.commands.registerCommand('vscode-go-by-example.helloWorld', async () => {
+		vscode.commands.registerCommand('vscode-go-by-example.openExample', async () => {
 
 			const indexContent = fs.readFileSync(vscode.Uri.file(path.join(context.extensionPath, 'media', 'index.html')).fsPath, 'utf8');
 			const itemRegex = new RegExp('<li><a href="([^"]*)">([^<]*)<', "g");
@@ -53,7 +36,6 @@ export function activate(context: vscode.ExtensionContext) {
 				vscode.ViewColumn.One,
 				{
 					// Only allow the webview to access resources in our extension's media directory
-					// localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'media'))]
 					localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'media')]
 				}
 			);
@@ -85,7 +67,6 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 	);
 }
-
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
