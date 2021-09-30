24516550d8e4057d4d789733460190d9150118f8
Stuart Leeks <stuart@leeks.net>
2021-09-30 20:03:54 +0000

    Tweak command name

--- ci-publish.sh
+++ ci-publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t vscode-go-by-example-devcontainer -f ./.devcontainer/Dockerfile ./.devcontainer
-docker run -v ${PWD}:/workspaces/vscode-go-by-example -e MARKETPLACE_PUBLISH_PAT --workdir /workspaces/vscode-go-by-example vscode-go-by-example-devcontainer bash -c "yarn install && yarn run publish"
+docker run -v ${PWD}:/workspaces/vscode-go-by-example -e MARKETPLACE_PUBLISH_PAT --workdir /workspaces/vscode-go-by-example vscode-go-by-example-devcontainer bash -c "yarn install && yarn package-web && yarn run publish"
